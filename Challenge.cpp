0c0119d2657f2e50ca06aead1d7771abc74ca52a
inputqqq <dimabropro68@gmail.com>
2025-12-08 16:45:44 +0300

    operator overload,operator<<

--- Challenge.cpp
+++ Challenge.cpp
@@ -1,21 +1,59 @@
 #include "Challenge.hpp"
 
-Challenge::Challenge(const std::string& id, const std::string& title, const std::string& description, int durationDays)
-    : id(id), title(title), description(description), durationDays(durationDays) {
+
+Challenge::Challenge() : durationDays(0) {}
+Challenge::Challenge(const std::string& id_, const std::string& t_, const std::string& desc_, int dur)
+	: id(id_), title(t_), description(desc_), durationDays(dur) {
 }
+
+
+Challenge::Challenge(const Challenge& other)
+	: id(other.id), title(other.title), description(other.description), durationDays(other.durationDays), participants(other.participants), progressMap(other.progressMap) {
+}
+
+
+Challenge Challenge::createChallenge(const std::string& id_, const std::string& t_, const std::string& desc_, int dur) {
+	return Challenge(id_, t_, desc_, dur);
+}
+
 
 void Challenge::join(const std::string& userId) {
-    participants.push_back(userId);
+	participants.push_back(userId);
+	progressMap[userId] = 0.0;
 }
 
-double Challenge::getProgress(const std::string&) const {
-    return 65.0; // заглушка
+
+void Challenge::setProgress(const std::string& userId, double value) {
+	progressMap[userId] = value;
 }
+
+
+double Challenge::getProgress(const std::string& userId) const {
+	auto it = progressMap.find(userId);
+	if (it != progressMap.end()) return it->second;
+	return 0.0;
+}
+
 
 void Challenge::showChallengeInfo() const {
-    std::cout << "=== Челлендж ===\n";
-    std::cout << "Название: " << title << "\n";
-    std::cout << "Описание: " << description << "\n";
-    std::cout << "Длительность: " << durationDays << " дней\n";
-    std::cout << "Участников: " << participants.size() << "\n\n";
+	std::cout << "Challenge: " << title << " (" << id << ")"
+		<< " Description: " << description << ""
+		<< " Duration: " << durationDays << " days"
+		<< " Participants: " << participants.size() << "";
+}
+
+
+Challenge& Challenge::operator+=(const Challenge& rhs) {
+	// combine participants (avoid duplicates simply)
+	for (const auto& p : rhs.participants) {
+		participants.push_back(p);
+		progressMap[p] = rhs.progressMap.at(p);
+	}
+	return *this;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Challenge& c) {
+	os << c.title << "(" << c.id << ")";
+	return os;
 }
--- Challenge.hpp
+++ Challenge.hpp
@@ -1,24 +1,43 @@
 #ifndef CHALLENGE_HPP
 #define CHALLENGE_HPP
 
+
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <iostream>
 
-class Challenge {
-private:
-    std::string id;
-    std::string title;
-    std::string description;
-    int durationDays;
-    std::vector<std::string> participants;
 
+class Challenge {
 public:
-    Challenge(const std::string& id, const std::string& title, const std::string& description, int durationDays);
-    void join(const std::string& userId);
-    double getProgress(const std::string& userId) const;
-    void showChallengeInfo() const;
-    ~Challenge() = default;
+	std::string id;
+	std::string title;
+	std::string description;
+	int durationDays{ 0 };
+	std::vector<std::string> participants;
+	std::unordered_map<std::string, double> progressMap;
+
+
+	Challenge();
+	Challenge(const std::string& id_, const std::string& t_, const std::string& desc_, int dur);
+	Challenge(const Challenge& other);
+	~Challenge() = default;
+
+
+	static Challenge createChallenge(const std::string& id_, const std::string& t_, const std::string& desc_, int dur);
+	void join(const std::string& userId);
+	void setProgress(const std::string& userId, double value);
+	double getProgress(const std::string& userId) const;
+	void showChallengeInfo() const;
+
+
+	// operator overload: add participants from another challenge
+	Challenge& operator+=(const Challenge& rhs);
+
+
+	// friend to print
+	friend std::ostream& operator<<(std::ostream& os, const Challenge& c);
 };
+
 
 #endif // CHALLENGE_HPP
