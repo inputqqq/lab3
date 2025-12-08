20d76db7c8e2b96156a6ea8277a89657912edb03
inputqqq <dimabropro68@gmail.com>
2025-12-08 16:42:59 +0300

    operator+,this,Copy constructor,friend func

--- CommunityProfile.cpp
+++ CommunityProfile.cpp
@@ -1,21 +1,50 @@
 ﻿#include "CommunityProfile.hpp"
 
-CommunityProfile::CommunityProfile(const std::string& id, const std::string& nickname, const std::string& joinedDate)
-    : id(id), nickname(nickname), joinedDate(joinedDate) {
+
+CommunityProfile::CommunityProfile() = default;
+CommunityProfile::CommunityProfile(const std::string& id_, const std::string& nick_, const std::string& joined)
+	: id(id_), nickname(nick_), joinedDate(joined) {
 }
+
+
+CommunityProfile::CommunityProfile(const CommunityProfile& other)
+	: id(other.id), nickname(other.nickname), joinedDate(other.joinedDate), friends(other.friends), badges(other.badges) {
+}
+
+
+CommunityProfile CommunityProfile::createProfile(const std::string& id_, const std::string& nick_, const std::string& joined) {
+	return CommunityProfile(id_, nick_, joined);
+}
+
 
 void CommunityProfile::addFriend(const std::string& friendId) {
-    friends.push_back(friendId);
+	friends.push_back(friendId);
 }
 
-void CommunityProfile::awardBadge(const std::string& badge) {
-    badges.push_back(badge);
+
+void CommunityProfile::awardBadge(const Achievement& badge) {
+	badges.push_back(badge);
 }
+
 
 void CommunityProfile::showProfileInfo() const {
-    std::cout << "=== Профиль ===\n";
-    std::cout << "ID: " << id << "\n";
-    std::cout << "Ник: " << nickname << "\n";
-    std::cout << "Регистрация: " << joinedDate << "\n";
-    std::cout << "Друзей: " << friends.size() << ", Достижений: " << badges.size() << "\n\n";
+	std::cout << "Profile: " << nickname << " (" << id << ")"
+		<< " Joined: " << joinedDate << ""
+		<< " Friends: " << friends.size() << " Badges: " << badges.size() << "";
+}
+
+
+// Example operator+: combines nicknames with '+' (toy example demonstrating operator overloading)
+CommunityProfile CommunityProfile::operator+(const CommunityProfile& rhs) const {
+	CommunityProfile res;
+	res.id = this->id + "+" + rhs.id;
+	res.nickname = this->nickname + "+" + rhs.nickname; // string concat (feature: std::string usage)
+	res.joinedDate = this->joinedDate; // preserve left date
+	return res;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const CommunityProfile& p) {
+	os << p.nickname << "(" << p.id << ")";
+	return os;
 }
--- CommunityProfile.hpp
+++ CommunityProfile.hpp
@@ -1,24 +1,43 @@
 #ifndef COMMUNITYPROFILE_HPP
 #define COMMUNITYPROFILE_HPP
 
+
 #include <string>
 #include <vector>
+#include <memory>
 #include <iostream>
+#include "Achievement.hpp"
+
 
 class CommunityProfile {
-private:
-    std::string id;
-    std::string nickname;
-    std::string joinedDate; // строка вместо std::tm
-    std::vector<std::string> friends;
-    std::vector<std::string> badges;
-
 public:
-    CommunityProfile(const std::string& id, const std::string& nickname, const std::string& joinedDate);
-    void addFriend(const std::string& friendId);
-    void awardBadge(const std::string& badge);
-    void showProfileInfo() const;
-    ~CommunityProfile() = default;
+	std::string id;
+	std::string nickname;
+	std::string joinedDate;
+	std::vector<std::string> friends; // store friend ids
+	std::vector<Achievement> badges;
+
+
+	CommunityProfile();
+	CommunityProfile(const std::string& id_, const std::string& nick_, const std::string& joined);
+	// Copy constructor (feature)
+	CommunityProfile(const CommunityProfile& other);
+	~CommunityProfile() = default;
+
+
+	static CommunityProfile createProfile(const std::string& id_, const std::string& nick_, const std::string& joined);
+	void addFriend(const std::string& friendId);
+	void awardBadge(const Achievement& badge);
+	void showProfileInfo() const;
+
+
+	// operator overload: concatenate nicknames (example)
+	CommunityProfile operator+(const CommunityProfile& rhs) const; // returns a combined profile (demo only)
+
+
+	// friend output
+	friend std::ostream& operator<<(std::ostream& os, const CommunityProfile& p);
 };
+
 
 #endif // COMMUNITYPROFILE_HPP
