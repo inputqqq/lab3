b32c1ef9d4e2846553a1c09c1ba389ee646b441b
inputqqq <dimabropro68@gmail.com>
2025-12-12 18:16:37 +0700

    realized:constructors/destructor,copy constructor,deep note assignment,virtual function — redefinition,surface/deep copying

--- CommunityProfile.cpp
+++ CommunityProfile.cpp
@@ -1,50 +1,91 @@
-﻿#include "CommunityProfile.hpp"
+﻿// CommunityProfile.cpp
+#include "CommunityProfile.hpp"
 
+// Конструкторы/деструктор
+CommunityProfile::CommunityProfile() : note(new std::string("no-note")) {}
 
-CommunityProfile::CommunityProfile() = default;
 CommunityProfile::CommunityProfile(const std::string& id_, const std::string& nick_, const std::string& joined)
-	: id(id_), nickname(nick_), joinedDate(joined) {
+    : id(id_), nickname(nick_), joinedDate(joined), note(new std::string("default-note")) {
 }
 
-
+// Copy constructor — делает глубокую копию note (так удобнее по умолчанию)
 CommunityProfile::CommunityProfile(const CommunityProfile& other)
-	: id(other.id), nickname(other.nickname), joinedDate(other.joinedDate), friends(other.friends), badges(other.badges) {
+    : id(other.id), nickname(other.nickname), joinedDate(other.joinedDate),
+    friends(other.friends), badges(other.badges), note(new std::string(*(other.note))) {
+    // Комментарий: реализация конструктора копирования (feature)
 }
 
+// operator= — корректно реализуем (с проверкой self-assign)
+CommunityProfile& CommunityProfile::operator=(const CommunityProfile& other) {
+    if (this == &other) return *this;
+    id = other.id;
+    nickname = other.nickname;
+    joinedDate = other.joinedDate;
+    friends = other.friends;
+    badges = other.badges;
+    // глубокое присваивание note
+    if (note) delete note;
+    note = new std::string(*(other.note));
+    return *this;
+}
 
+CommunityProfile::~CommunityProfile() {
+    // удаляем выделенную память для note
+    delete note;
+}
+
+// фабрика
 CommunityProfile CommunityProfile::createProfile(const std::string& id_, const std::string& nick_, const std::string& joined) {
-	return CommunityProfile(id_, nick_, joined);
+    return CommunityProfile(id_, nick_, joined);
 }
-
 
 void CommunityProfile::addFriend(const std::string& friendId) {
-	friends.push_back(friendId);
+    friends.push_back(friendId);
 }
-
 
 void CommunityProfile::awardBadge(const Achievement& badge) {
-	badges.push_back(badge);
+    badges.push_back(badge);
 }
 
-
+// Виртуальная функция — переопределение
 void CommunityProfile::showProfileInfo() const {
-	std::cout << "Profile: " << nickname << " (" << id << ")"
-		<< " Joined: " << joinedDate << ""
-		<< " Friends: " << friends.size() << " Badges: " << badges.size() << "";
+    std::cout << "Profile: " << nickname << " (" << id << ")\n"
+        << "  Joined: " << joinedDate << "\n"
+        << "  Friends: " << friends.size() << "  Badges: " << badges.size() << "\n"
+        << "  Note: " << (note ? *note : std::string("<no note>")) << "\n";
 }
 
-
-// Example operator+: combines nicknames with '+' (toy example demonstrating operator overloading)
+// operator+ (простой пример, использует this)
 CommunityProfile CommunityProfile::operator+(const CommunityProfile& rhs) const {
-	CommunityProfile res;
-	res.id = this->id + "+" + rhs.id;
-	res.nickname = this->nickname + "+" + rhs.nickname; // string concat (feature: std::string usage)
-	res.joinedDate = this->joinedDate; // preserve left date
-	return res;
+    CommunityProfile res;
+    res.id = this->id + "+" + rhs.id;
+    res.nickname = this->nickname + "+" + rhs.nickname;
+    res.joinedDate = this->joinedDate;
+    // Пример: не копируем note — оставляем дефолт (демо)
+    return res;
 }
-
 
 std::ostream& operator<<(std::ostream& os, const CommunityProfile& p) {
-	os << p.nickname << "(" << p.id << ")";
-	return os;
+    os << p.nickname << "(" << p.id << ")";
+    return os;
+}
+
+// Клонирование:
+// поверхностное: копирует указатель note (shallow copy)
+ProfileBase* CommunityProfile::cloneShallow() const {
+    CommunityProfile* cp = new CommunityProfile();
+    cp->id = id;
+    cp->nickname = nickname;
+    cp->joinedDate = joinedDate;
+    cp->friends = friends;
+    cp->badges = badges;
+    // поверхностное копирование указателя — оба объекта будут указывать на одну и ту же строку
+    cp->note = this->note;
+    return cp;
+}
+
+// глубокое: создаёт новый note с копированием содержимого
+ProfileBase* CommunityProfile::cloneDeep() const {
+    CommunityProfile* cp = new CommunityProfile(*this); // copy ctor уже делает глубокую копию note
+    return cp;
 }
